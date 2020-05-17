fc011680f7e3c61b9496a8848a77cab83fb01997
jonisb <github.com@JsBComputing.se>
2020-05-17 17:17:51 +0200

    Create test_network.py

---
+++ tests/test_network.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals, division, absolute_import
+
+import unittest
+
+
+class test_network(unittest.TestCase):
+    def test_network(self):
+        import jsbc.network
