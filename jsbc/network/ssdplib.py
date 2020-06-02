b1811c9f45ec42754d2fd4257abd36b9f1985dd8
jonisb <github.com@JsBComputing.se>
2020-06-02 22:33:03 +0200

    Start of ssdplib with tests

---
+++ jsbc/network/ssdplib.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals, division, absolute_import
+
+import http.client
+import io
+import uuid
+import collections
+import logging
+
+logger = logging.getLogger(__name__)
+
+address = collections.namedtuple('address', ['host', 'port'])
+ssdpaddr = address('239.255.255.250', 1900)
+
+
+class _FakeSocket(io.BytesIO):
+    def makefile(self, *args, **kw):
+        return self
+
+
+class BASE():
+    def __str__(self):
+        headers = [self.requestline]
+        #for header in self:
+        #    headers.append(f"{header}: {self[header]}")
+        for header in self.headers:
+            #headers.append(f"{header}: {self.headers[header]}")
+            headers.append("{0}: {1}".format(header, self.headers[header]))
+        return "\r\n".join(headers + ['\r\n'])
+
+    def __repr__(self):
+        #return f"{self.addr}\n{self}"
+        return "{0}\n{1}".format(self.addr, self)
+
+    def __bytes__(self):
+        return self.__str__().encode('latin-1')
+
+
+class REQUEST(BASE):
+    def __init__(self, message=None, addr=None, method='M-SEARCH', path='*', version='HTTP/1.1'):
+        if message is not None:
+            message = message.rstrip().splitlines()
+            self.requestline = message[0].decode('latin-1')
+            self.method, self.path, self.version = self.requestline.split()
+            if self.method in ('M-SEARCH', 'NOTIFY'):
+                headers = b"\r\n".join(message[1:])
+                self.headers = http.client.parse_headers(_FakeSocket(headers))
+                try:
+                    self.id = uuid.UUID(self.headers['USN'].split('::')[0])
+                except AttributeError:
+                    self.id = None
+            else:
+                raise StopIteration
+        else:
+            if method in ('M-SEARCH', 'NOTIFY'):
+                self.id = None
+                self.method, self.path, self.version = method, path, version
+                #self.requestline = f'{method} {path} {version}'
+                self.requestline = '{0} {1} {2}'.format(method, path, version)
+                self.build()
+            else:
+                raise StopIteration
+        try:
+            self.addr = address(*addr)
+        except TypeError:
+            self.addr = None
+
+    def build(self, service='rootdevice', library='upnp', host=ssdpaddr.host, port=ssdpaddr.port, mx=3):
+        #self.headers = {'st': f'{library}:{service}', 'mx': mx, 'man': '"ssdp:discover"', 'host': f'{host}:{port}'}
+        self.headers = {'st': '{0}:{1}'.format(library, service), 'mx': mx, 'man': '"ssdp:discover"', 'host': '{0}:{1}'.format(host, port)}
+        return self
+
+
+if __name__ == '__main__':
+    import pprint
+
+    request = REQUEST()
+
+    print(request)
+    print(repr(request))
---
+++ tests/test_ssdp.py
@@ -0,0 +1,65 @@
+﻿# -*- coding: utf-8 -*-
+import unittest
+
+import uuid
+#from jsbc.network.ssdplib import REQUEST, REPLY, SSDP
+from jsbc.network.ssdplib import REQUEST
+
+
+class test_testing(unittest.TestCase):
+    def test_REQUEST_no_data(self):
+        request = REQUEST()
+        assert request.id == None
+        assert request.addr == None
+        assert request.requestline == u'M-SEARCH * HTTP/1.1'
+        #assert REQUEST() == {u'st': u'upnp:rootdevice', u'mx': u'3', u'man': u'"ssdp:discover"', u'host': u'239.255.255.250:1900'}
+        assert bytes(request).splitlines()[0] == b'M-SEARCH * HTTP/1.1'
+        assert {line.partition(b': ')[0]: line.partition(b': ')[2] for line in bytes(request).splitlines()[1:]} == {b'st': b'upnp:rootdevice', b'mx': b'3', b'man': b'"ssdp:discover"', b'host': b'239.255.255.250:1900', b'': b''}
+
+    def test_REQUEST_with_data(self):
+        request = REQUEST(bytes(REQUEST()))
+
+        assert request.id == None
+        assert request.addr == None
+        assert request.requestline == u'M-SEARCH * HTTP/1.1'
+        #assert request == {u'st': u'upnp:rootdevice', u'mx': u'3', u'man': u'"ssdp:discover"', u'host': u'239.255.255.250:1900'}, repr(request)
+        assert bytes(request).splitlines()[0] == b'M-SEARCH * HTTP/1.1'
+        assert {line.partition(b': ')[0]: line.partition(b': ')[2] for line in bytes(request).splitlines()[1:]} == {b'st': b'upnp:rootdevice', b'mx': b'3', b'man': b'"ssdp:discover"', b'host': b'239.255.255.250:1900', b'': b''}
+
+
+    '''
+    def test_REPLY_with_data(self):
+        test_reply = b"""\
+HTTP/1.1 200 OK\r
+CACHE-CONTROL: max-age=120\r
+ST: upnp:rootdevice\r
+USN: uuid:e8d31334-41cd-4a9f-8950-bce16ff38738::upnp:rootdevice\r
+EXT:\r
+SERVER: ASUSTeK UPnP/1.0 MiniUPnPd/1.4\r
+LOCATION: http://192.168.1.1:45701/rootDesc.xml\r
+\r
+"""
+        reply = REPLY(test_reply, ('192.168.1.1', 1900))
+
+        assert reply.statusline == u'HTTP/1.1 200 OK'
+        assert reply.version == u'HTTP/1.1'
+        assert reply.code == u'200'
+        assert reply.codemsg == u'OK'
+
+        assert reply.id == uuid.UUID('uuid:e8d31334-41cd-4a9f-8950-bce16ff38738'), reply.id
+        assert reply.addr == ('192.168.1.1', 1900)
+        assert reply[u'st'] == u'upnp:rootdevice', repr(reply[u'st'])
+        assert reply[u'usn'] == u'uuid:e8d31334-41cd-4a9f-8950-bce16ff38738::upnp:rootdevice'
+        assert reply[u'server'] == u'ASUSTeK UPnP/1.0 MiniUPnPd/1.4'
+        assert reply[u'location'] == u'http://192.168.1.1:45701/rootDesc.xml'
+        assert reply[u'cache-control'] == u'max-age=120'
+        assert reply[u'ext'] == u'', repr(reply[u'ext'])
+        #assert reply == {u'st': u'upnp:rootdevice', u'usn': u'uuid:e8d31334-41cd-4a9f-8950-bce16ff38738::upnp:rootdevice', u'server': u'ASUSTeK UPnP/1.0 MiniUPnPd/1.4', u'location': u'http://192.168.1.1:45701/rootDesc.xml', u'cache-control': u'max-age=120', u'ext': u''}, repr(reply)
+        #assert bytes(reply).splitlines()[0] == b'HTTP/1.1 200 OK', bytes(reply)
+        #assert {line.partition(b': ')[0]: line.partition(b': ')[2] for line in bytes(reply).splitlines()[1:]} == {b'st': b'upnp:rootdevice', b'usn': b'uuid:e8d31334-41cd-4a9f-8950-bce16ff38738::upnp:rootdevice', b'server': b'ASUSTeK UPnP/1.0 MiniUPnPd/1.4', b'location': b'http://192.168.1.1:45701/rootDesc.xml', b'cache-control': b'max-age=120', b'ext': b'', b'': b''}
+
+    '''
+    #def test_ssdplibclass(self):
+    #    ssdp = SSDP()
+
+    #    handleRequestQueue()
